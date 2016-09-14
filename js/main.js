ace722edc16f084b3405084e5f8d485c3f0c58cb
byOPKY <orrillokarol@gmail.com>
2016-09-14 14:05:08 -0500

    subiendo

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementById("form-signin-heading").innerHTML ="Por favor inicie Sesion";
+document.getElementById("inputEmail").placeholder="Correo Electronico";
+document.getElementById("inputPassword").placeholder="Contraseña";
+document.querySelector("span").innerHTML ="Recordar datos";
+document.querySelector("button").innerHTML ="Iniciar Sesion";
